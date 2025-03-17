ebb174f8b3f3a221fd1f593a89d16b37d07d4c69
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-03-17 23:22:14 +0100

    importing hamburher menu

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import styles from './navigation.module.css'
 import { uniqueCategories } from '../../data/data'
 import NavMenu from '../NavMenu'
+import Hamburger from 'hamburger-react'
 
 const Navigation = ({selectedPage,selectedProduct}) => {
     const handleClick = (item) => {
@@ -13,11 +15,29 @@ const Navigation = ({selectedPage,selectedProduct}) => {
         selectedProduct(null)
     }
 
+    const [isOpen, setIsOpen] = useState(false)
+    
     return (
-
         <div className={styles.nav}>
+            <div className={styles.hamburger}>
+            <Hamburger toggled={isOpen} toggle={setIsOpen} />
+        </div>
+
+        {isOpen && 
+            <>
+             <div className={styles.hamburger_links}>
+            <NavMenu menu = "Home" onClick={handleHome} />
+            {uniqueCategories.map((item, index) => (
+            <NavMenu key={index} menu = {item} onClick={()=>handleClick(item)} />
+            ))}
+            </div>
+            </>
+        }
+
+        <div className={styles.links}>
             <NavMenu menu= "Home" onClick={handleHome} />
             {uniqueCategories.map((item,index)=> <NavMenu key={index} menu={item} onClick={()=>handleClick(item)}/>)}
+        </div>
         </div>
     )
 }
