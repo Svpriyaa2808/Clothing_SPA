b1f863fa1bfa3656194118c4a872460a5b133e55
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-03-24 22:41:03 +0100

    adding home page content

--- src/components/HomeMain/index.jsx
+++ src/components/HomeMain/index.jsx
@@ -1,8 +1,26 @@
+import styles from './homemain.module.css'
+import { womenCategories  } from '../../data/data'
+import { getImageUrl } from '../../utils/function'
+import HomeImage from '../../assets/home.jpeg'
 const HomeMain = () => {
     return (
-        <div>
-            <p>Home</p>
-        </div>
+        <>
+        
+                        <div className={styles.home_image_container}>
+                            <img src={HomeImage} alt='spring_coolection'></img>
+                        </div>
+                        <div className={styles.limited_collection_info}>
+                            <h2>Limited Edition</h2>
+                            <p>A truly unique party collection. Catch them before they float away! In this limited edition collection,
+                                 fluffy dresses and powdery pastels meet in sheer materials that dance with every movement, carefully crafted for both big and small dreams. Psst... There are only a few available, so don't miss out on these treasures!</p>
+                        </div>
+                        <div className={styles.spring_collection_details}>
+                            <div className={styles.spring_collection_image}></div>
+                            <div className={styles.spring_collection_info}></div>
+                        </div>
+                    
+                    
+     </>
     )
 }
 
